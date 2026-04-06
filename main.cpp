ec3765da063ff4da441f117f9e9eb2cb81b2d9b5
Barsbek Azamatov <bars.moderside@gmail.com>
2026-04-05 20:43:24 -0700

    update

--- main.cpp
+++ main.cpp
@@ -2,13 +2,25 @@
 #include <chrono>
 #include <fstream>
 using namespace std;
-using namespace std::chrono;
+using namespace chrono;
 
 int main() {
     string filename{"codes.txt"};
 
-    
+    ifstream fileInput{"codes.txt"};
+    if (!fileInput) {
+        cerr << "File not found";
+        return 1;
+    }
+
     return 0;
+}
+
+long inputVector(const vector<string>& data) {
+    auto start{high_resolution_clock::now()};
+    vector<string> vec{data.begin(), data.end()};
+    auto end{high_resolution_clock::now()};
+    return duration_cast<nanoseconds>(end - start).count();
 }
 
 /* syntax examples:
